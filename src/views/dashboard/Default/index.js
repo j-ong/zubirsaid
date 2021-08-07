17925aa721ee8badfeed0192995ed1e0574b2319
j-ong <49931213+j-ong@users.noreply.github.com>
2021-08-07 23:37:14 +0800

    updated zs biography on dashboard

--- src/views/dashboard/Default/index.js
+++ src/views/dashboard/Default/index.js
@@ -5,6 +5,8 @@ import { Grid } from '@material-ui/core';
 
 // project imports
 import TotalNodesCard from './TotalNodesCard';
+import ProfileCard from './ProfileCard';
+
 import PopularCard from './PopularCard';
 import TotalOrderLineChartCard from './TotalOrderLineChartCard';
 import TotalIncomeDarkCard from './TotalIncomeDarkCard';
@@ -47,28 +49,19 @@ const Dashboard = () => {
                     <Grid item lg={4} md={6} sm={6} xs={12}>
                         <TotalNodesCard isLoading={isLoading} songsCount={counter} />
                     </Grid>
-                    <Grid item lg={4} md={6} sm={6} xs={12}>
+
+                    <Grid item lg={8} md={6} sm={6} xs={12}>
                         <TotalOrderLineChartCard isLoading={isLoading} />
-                    </Grid>
-                    <Grid item lg={4} md={12} sm={12} xs={12}>
-                        <Grid container spacing={gridSpacing}>
-                            <Grid item sm={6} xs={12} md={6} lg={12}>
-                                <TotalIncomeDarkCard isLoading={isLoading} />
-                            </Grid>
-                            <Grid item sm={6} xs={12} md={6} lg={12}>
-                                <TotalIncomeLightCard isLoading={isLoading} />
-                            </Grid>
-                        </Grid>
                     </Grid>
                 </Grid>
             </Grid>
             <Grid item xs={12}>
                 <Grid container spacing={gridSpacing}>
+                    <Grid item xs={12} md={4}>
+                        <ProfileCard isLoading={isLoading} />
+                    </Grid>
                     <Grid item xs={12} md={8}>
                         <TotalGrowthBarChart isLoading={isLoading} />
-                    </Grid>
-                    <Grid item xs={12} md={4}>
-                        <PopularCard isLoading={isLoading} />
                     </Grid>
                 </Grid>
             </Grid>
