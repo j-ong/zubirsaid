cb624bd6b986f268b98a359cffbbe0e2ca87ac0c
Kelvin Hew <69758273+KelvinHew@users.noreply.github.com>
2021-08-16 19:39:36 +0800

    Added Tab portion for each node, as well as mock cytoscape segment in new tabbed area.

--- src/views/node/index.js
+++ src/views/node/index.js
@@ -10,7 +10,10 @@ import MainlistContext from '../../contexts/mainlist/mainlistContext';
 import NodePropertyItem from './NodePropertyItem';
 
 // material-ui
-import { Grid } from '@material-ui/core';
+import { Grid, AppBar, Tabs, Tab, Box } from '@material-ui/core';
+import {} from '@material-ui/core/styles';
+
+import {TabContext, TabPanel,TabList} from '@material-ui/lab';
 // import { Typography } from '@material-ui/core';
 // import MuiTypography from '@material-ui/core/Typography';
 
@@ -19,11 +22,23 @@ import MainCard from '../../ui-component/cards/MainCard';
 import SubCard from './../../ui-component/cards/SubCard';
 import { gridSpacing } from './../../store/constant';
 
+//Cytoscape components
+import retrieveInfo from '../../connectionObject/connectionObject';
+import cytoscape from 'cytoscape';
+import CytoscapeComponent from 'react-cytoscapejs/src/component';
+import Cytoscape from './CytoscapeComponent';
+
 //==============================|| SAMPLE PAGE ||==============================//
 
 const Node = ({ match }) => {
     const mainlistContext = useContext(MainlistContext);
     const { nodes, getNodes, nodeSummary, loading } = mainlistContext;
+    const [value, setValue] = React.useState("0");
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
 
     useEffect(() => {
         getNodes(match.params.id);
@@ -32,6 +47,14 @@ const Node = ({ match }) => {
 
     return (
         <MainCard title={nodeSummary.label}>
+            <TabContext value={value}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <TabList onChange={handleChange} position="static">
+                <Tab label="Main Information" value="0" />
+                <Tab label="Graphic Visualization" value="1" />
+            </TabList>
+            </Box>
+            <TabPanel value="0">
             <Grid container spacing={gridSpacing}>
                 {nodes.map((node) => (
                     <Grid item xs={12} sm={12} key={node.group}>
@@ -47,6 +70,26 @@ const Node = ({ match }) => {
                     </Grid>
                 ))}
             </Grid>
+            </TabPanel>
+            <TabPanel value="1">
+                <h2>Cytoscape:</h2>
+                <Cytoscape
+                    height={200}
+                    width={200}
+                    elements={CytoscapeComponent.normalizeElements({
+                        nodes: [
+                            { data: { id: 'node_' + "test", label: "main_node"}, position: { x: 50, y: 100 } },
+                            { data: { id: 'two', label: 'Node 2' }, position: { x: 150, y: 100 } }
+                        ],
+                        edges: [
+                            {
+                                data: { source: 'node_' + "test", target: 'two', label: 'Edge from Node1 to Node2' }
+                            }
+                        ]
+                    })}
+                />
+            </TabPanel>
+            </TabContext>
         </MainCard>
     );
 };
